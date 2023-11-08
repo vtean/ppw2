cbd68e59466e9a37aa6d5e75b5361911c0b2fdf6
Valentin T <valentintean@gmail.com>
2023-11-08 18:09:59 +0200

    add crud routes for specialties

--- node_express/controllers/specialties.js
+++ node_express/controllers/specialties.js
@@ -3,9 +3,41 @@ const db = require("../db/db.json");
 
 // get specialties
 const getSpecialties = (req, res) => {
-    res.send(db.specialties);
+    res.json(db.specialties);
+};
+
+// get specialties by id
+const getSpecialtiesById = (req, res) => {
+    const id = req.params.id;
+    const specialty = db.specialties.find((specialty) => specialty.id == id);
+    res.json(specialty);
+};
+
+// add specialty
+const addSpecialty = (req, res) => {
+    const specialty = req.body;
+    db.specialties.push(specialty);
+    res.json(db.specialties);
+};
+
+// update specialty
+const updateSpecialty = (req, res) => {
+    const specialtyIndex = db.specialties.findIndex((specialty) => specialty.id == req.params.id);
+    db.specialties.splice(specialtyIndex, 1, req.body);
+    res.json(req.body);
+};
+
+// delete specialty
+const deleteSpecialty = (req, res) => {
+    const specialtyIndex = db.specialties.findIndex((specialty) => specialty.id == req.params.id);
+    db.specialties.splice(specialtyIndex, 1);
+    res.json({});
 };
 
 module.exports = {
     getSpecialties,
+    getSpecialtiesById,
+    addSpecialty,
+    updateSpecialty,
+    deleteSpecialty,
 };
--- node_express/routes/specialtiesRoutes.js
+++ node_express/routes/specialtiesRoutes.js
@@ -1,8 +1,18 @@
 const express = require("express");
-const { getSpecialties } = require("../controllers/specialties.js");
+const {
+    getSpecialties,
+    getSpecialtiesById,
+    addSpecialty,
+    updateSpecialty,
+    deleteSpecialty,
+} = require("../controllers/specialties.js");
 
 const router = express.Router();
 
 router.get("/", getSpecialties);
+router.get("/:id", getSpecialtiesById);
+router.post("/", addSpecialty);
+router.put("/:id", updateSpecialty);
+router.delete("/:id", deleteSpecialty);
 
 module.exports = router;
